0fbf64faaffc1ca7df89b5440bf98b9059c53510
Abbosbek123 <azamatabbos@gmail.com>
2022-01-29 10:56:28 -0500

    Sortingda array lis

--- lambda/src/main/java/task/OrangeTest.java
+++ lambda/src/main/java/task/OrangeTest.java
@@ -12,11 +12,22 @@ public class OrangeTest {
       //  inventory.add(Orange.builder().weight(200).build()); // We can skip the constructor value
         inventory.add(Orange.builder().weight(500).color(Color.RED).build());
         inventory.add(Orange.builder().weight(300).color(Color.GREEN).build());
+
+        OrangeFormatter simpleFormatter= orange -> "An orange of "+ orange.getWeight()+"g";
+     //   prettyPrintOrange(inventory,simpleFormatter);
+
+        prettyPrintOrange(inventory,  orange -> "An orange of "+ orange.getWeight()+"g");
+        System.out.println("****************************************");
+        OrangeFormatter fancyFormatter= orange -> {
+            String characteristic= orange.getWeight() >150 ? "Heavy": "Light";
+            return "A "+characteristic+" "+orange.getColor()+" oranges";
+        };
+        prettyPrintOrange(inventory,fancyFormatter);
     }
 
 
 
-    private static void prettyPrintApple(List<Orange> inventory,OrangeFormatter orangeFormatter){
+    private static void prettyPrintOrange(List<Orange> inventory,OrangeFormatter orangeFormatter){
         for(Orange orange : inventory){
             String output = orangeFormatter.accept(orange);
             System.out.println(output);
