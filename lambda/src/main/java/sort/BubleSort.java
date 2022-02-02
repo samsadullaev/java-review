4c0d2f2fdf4fad06364fc6c1e963f1a3ad1a4a0c
Abbosbek123 <azamatabbos@gmail.com>
2022-02-01 19:52:45 -0500

    Orangzi class

---
+++ lambda/src/main/java/sort/BubleSort.java
@@ -0,0 +1,8 @@
+package sort;
+
+public class BubleSort  implements Sorting {
+    @Override
+    public void sort() {
+        System.out.println("Buble sorting");
+    }
+}
---
+++ lambda/src/main/java/sort/Sorting.java
@@ -0,0 +1,7 @@
+package sort;
+
+@FunctionalInterface //optional anannations
+public interface Sorting {
+    public void sort();
+
+}
