d4360fac6e65a6603b9490c31c7609341cccf158
Abbosbek123 <azamatabbos@gmail.com>
2022-02-01 19:53:34 -0500

    Comparator Demo class is create

--- lambda/src/main/java/comparatorinterface/ComparatorDemo.java
+++ lambda/src/main/java/comparatorinterface/ComparatorDemo.java
@@ -2,7 +2,10 @@ package comparatorinterface;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+
+import static java.util.Comparator.comparing;
 
 public class ComparatorDemo {
     public static void main(String[] args) {
@@ -26,5 +29,30 @@ public class ComparatorDemo {
         //Descending Order
         list.sort( (o2, o1) -> o1.compareTo(o2) );
         System.out.println(list);
+
+
+        List<Apple> myInventory = Arrays.asList(
+                new Apple(80,Color.GREEN),
+                new Apple(155,Color.RED),
+                new Apple(120,Color.GREEN)
+        );
+
+        Comparator<Apple> sortApple = comparing((apple) -> apple.getWeight());
+        myInventory.sort(sortApple);
+        System.out.println(myInventory);
+
+        myInventory.sort(comparing((apple) -> apple.getWeight()));
+
+        myInventory.sort(comparing(Apple ::getWeight));
+
+        //Reversed
+        myInventory.sort(comparing(Apple::getWeight).reversed());
+
+        //Chaining
+        myInventory.
+                        sort(comparing(Apple::getWeight).
+                        reversed().
+                        thenComparing(Apple::getColor));
+        System.out.println(myInventory);
     }
 }
