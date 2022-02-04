b23ac04745d2423046504ae47eeec31b8597efd5
Abbosbek123 <azamatabbos@gmail.com>
2022-02-03 20:29:35 -0500

    init commit

--- stream/src/main/java/EmployeeTest.java
+++ stream/src/main/java/EmployeeTest.java
@@ -1,3 +1,5 @@
+import java.util.List;
+
 public class EmployeeTest {
     public static void main(String[] args) {
 
@@ -7,8 +9,21 @@ public class EmployeeTest {
                 .forEach(System.out::println);
 
         //Print all phone numbers
+        System.out.println("************************");
        EmployeeData.readlAll()
                .map(Employee::getEmpPhoneNumbers)
                .forEach(System.out::println);
+
+        System.out.println("************************");
+       EmployeeData.readlAll()
+               .flatMap(employee -> employee.getEmpPhoneNumbers().stream())
+               .forEach(System.out::println);
+
+        System.out.println("************************");
+        EmployeeData.readlAll()
+                .map(Employee::getEmpPhoneNumbers)
+                .flatMap(List::stream)
+                .forEach(System.out::println);
+
     }
 }
