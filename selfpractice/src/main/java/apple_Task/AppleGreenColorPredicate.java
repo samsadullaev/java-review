d722b1832c447b7fc541d120957e06f66d6ef411
Abbosbek123 <azamatabbos@gmail.com>
2022-02-03 22:21:04 -0500

    AppleGreenColorPredicate class is done

--- selfpractice/src/main/java/apple_Task/AppleGreenColorPredicate.java
+++ selfpractice/src/main/java/apple_Task/AppleGreenColorPredicate.java
@@ -1,4 +1,9 @@
 package apple_Task;
 
 public class AppleGreenColorPredicate implements ApplePredicate{
+    @Override
+    public boolean test(Apple apple) {
+        return apple.getColor().equals(Color.GREEN);
+        // return apple.getColor().
+    }
 }
